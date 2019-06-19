b6d4ad97cab6c408a924771377efa0461bea87ab
matan335 <matancoder114@gmail.com>
2019-06-19 09:09:18 +0300

    1 file modified

--- components/Controls.jsx
+++ components/Controls.jsx
@@ -5,27 +5,45 @@ import PlayerService from '../services/PlayerService'
 import { checkBouderies } from '../services/MapService'
 
 const Controls = props => {
-  const movePlayer = e => {
-    const dir = e.target.value
+
+  document.addEventListener('keydown', e => {
+    //console.log('key pressed', e)
+    e.preventDefault()
+
+    let key = e.key
+    if (typeof key === 'string' && key.includes('Arrow')) {
+      key = key.replace('Arrow', '').toLowerCase()
+    }
+    console.log(key)
+    // if (
+    //   key === 'up' ||
+    //   key === 'left' ||
+    //   key === 'right' ||
+    //   key === 'down'
+    // ) movePlayer(key)
+  })
+
+  const movePlayer = dir => {
+
+    //const dir = e.target.value
     const oldPos = props.playerState.pos
     const map = props.mapState.map
     const pos = PlayerService.getNewPos(oldPos, dir, map)
-    if(arraysEqual(oldPos,pos)) return
+    if (arraysEqual(oldPos, pos)) return
     const slutVal = map[pos[0]][pos[1]]
-    if(typeof slutVal ==='object' && slutVal.value === 20){
+    if (typeof slutVal === 'object' && slutVal.value === 20) {
       //console.log('hit enemy',slutVal, props)
       const enemy = props.entitiesState.enemies.find(en => en.id == slutVal.id)
-
       const player = props.playerState
 
       props.dispatch({
-        type:'ENTER_BATTLE',
+        type: 'ENTER_BATTLE',
         enemy,
         player
       })
     }
     if (!checkBouderies(map, pos)) props.dispatch({
-      type: 'SET_PLAYER_POS', pos, oldPos,dir
+      type: 'SET_PLAYER_POS', pos, oldPos, dir
     })
 
   }
@@ -33,16 +51,20 @@ const Controls = props => {
   return (
     <div style={styles.container}>
       <button style={styles.btnUp}
-        onClick={() => movePlayer(event)} value="up">up</button>
+        onClick={e => movePlayer(e.target.value)}
+        value="up">up</button>
 
       <button style={styles.btnDown}
-        onClick={() => movePlayer(event)} value="down">down</button>
+        onClick={e => movePlayer(e.target.value)}
+        value="down">down</button>
 
       <button style={styles.btnLeft}
-        onClick={() => movePlayer(event)} value="left">left</button>
+        onClick={e => movePlayer(e.target.value)}
+        value="left">left</button>
 
       <button style={styles.btnRight}
-        onClick={() => movePlayer(event)} value="right">right</button>
+        onClick={e => movePlayer(e.target.value)}
+        value="right">right</button>
     </div>
   )
 }
